6a1a2380b838c3f09469fd41e80b7ffc1e307907
Hossenata <104681172+Hossenata@users.noreply.github.com>
2025-05-16 17:53:58 +0300

    linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -1,49 +1,54 @@
-#linux-run.sh LINUX_USER_PASSWORD NGROK_AUTH_TOKEN LINUX_USERNAME LINUX_MACHINE_NAME
 #!/bin/bash
-# /home/runner/.ngrok2/ngrok.yml
 
-sudo useradd -m $LINUX_USERNAME
-sudo adduser $LINUX_USERNAME sudo
-echo "$LINUX_USERNAME:$LINUX_USER_PASSWORD" | sudo chpasswd
-sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-sudo hostname $LINUX_MACHINE_NAME
-
+# متغيرات مهمة
 if [[ -z "$NGROK_AUTH_TOKEN" ]]; then
   echo "Please set 'NGROK_AUTH_TOKEN'"
   exit 2
 fi
 
 if [[ -z "$LINUX_USER_PASSWORD" ]]; then
-  echo "Please set 'LINUX_USER_PASSWORD' for user: $USER"
+  echo "Please set 'LINUX_USER_PASSWORD'"
   exit 3
 fi
 
-echo "### Install ngrok ###"
+if [[ -z "$LINUX_USERNAME" ]]; then
+  echo "Please set 'LINUX_USERNAME'"
+  exit 4
+fi
 
-wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
-unzip ngrok-stable-linux-386.zip
-chmod +x ./ngrok
+if [[ -z "$LINUX_MACHINE_NAME" ]]; then
+  LINUX_MACHINE_NAME="ubuntu-vps"
+fi
 
-echo "### Update user: $USER password ###"
-echo -e "$LINUX_USER_PASSWORD\n$LINUX_USER_PASSWORD" | sudo passwd "$USER"
+echo "### إعداد المستخدم الجديد ###"
+sudo useradd -m "$LINUX_USERNAME"
+echo "$LINUX_USERNAME:$LINUX_USER_PASSWORD" | sudo chpasswd
+sudo usermod -aG sudo "$LINUX_USERNAME"
+sudo hostnamectl set-hostname "$LINUX_MACHINE_NAME"
 
-echo "### Start ngrok proxy for 22 port ###"
+echo "### تنزيل ngrok (إصدار حديث) ###"
+wget -O ngrok.zip https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.zip
+unzip ngrok.zip
+chmod +x ngrok
+sudo mv ngrok /usr/local/bin/ngrok
 
+echo "### تسجيل توكن ngrok ###"
+ngrok config add-authtoken "$NGROK_AUTH_TOKEN"
 
-rm -f .ngrok.log
-./ngrok authtoken "$NGROK_AUTH_TOKEN"
-./ngrok tcp 22 --log ".ngrok.log" &
-
+echo "### تشغيل ngrok على المنفذ 22 ###"
+ngrok tcp 22 > ngrok.log &
 sleep 10
-HAS_ERRORS=$(grep "command failed" < .ngrok.log)
+
+HAS_ERRORS=$(grep "ERR_" ngrok.log)
 
 if [[ -z "$HAS_ERRORS" ]]; then
   echo ""
   echo "=========================================="
-  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
-  echo "or conenct with $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh (Your Linux Username)@/" | sed "s/:/ -p /")"
+  echo "تم تشغيل السيرفر!"
+  echo "اتصل باستخدام الأمر التالي:"
+  echo "$(grep -o -E "tcp://(.+)" < ngrok.log | sed "s/tcp:\/\//ssh $LINUX_USERNAME@/" | sed "s/:/ -p /")"
   echo "=========================================="
 else
   echo "$HAS_ERRORS"
-  exit 4
+  exit 5
 fi
